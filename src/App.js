6da4580de371f79a027b6909ca6b271a10f3d874
GWeightman <g.weightman@hotmail.co.uk>
2022-01-10 11:59:24 +0000

    project start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React, { useState } from "react"
 import './App.css';
+import { Login } from "./components/login";
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState()
+  const [username, setUsername] = useState()
+  const [arr, setArr] = useState(["Steve", "Gary", "Clive", "Harry"])
+
+  const submit_handler = (e) => {
+    e.preventDefault()
+    setUsername(user)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{user}</h1>
+      {username ? <h1>Welcome {username}</h1> : <h1>Please log in</h1>}
+      {username && <h2>Success</h2>}
+      <Login setter={setUser} handler={submit_handler} />
+      {arr.map((item, i) => {
+        return(
+          <p key={i}>{item}</p>
+        )
+      })}
     </div>
   );
 }
