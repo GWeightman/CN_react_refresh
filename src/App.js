1205bd6b8a139318b162052ae2b336cf0d2c37af
GWeightman <g.weightman@hotmail.co.uk>
2022-01-10 13:43:09 +0000

    Updated

--- src/App.js
+++ src/App.js
@@ -1,30 +1,47 @@
-import React, { useState } from "react"
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 import { Login } from "./components/login";
 
 const App = () => {
-  const [user, setUser] = useState()
-  const [username, setUsername] = useState()
-  const [arr, setArr] = useState(["Steve", "Gary", "Clive", "Harry"])
+  const [user, setUser] = useState();
+  const [username, setUsername] = useState();
+  const [arr, setArr] = useState([]);
 
-  const submit_handler = (e) => {
-    e.preventDefault()
-    setUsername(user)
-  }
+  useEffect(() => {
+    fetchReq();
+  }, []);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setUsername(user);
+  };
+
+  const fetchReq = async () => {
+    const response = await fetch("https://picsum.photos/v2/list");
+    const data = await response.json();
+    setArr(data);
+  };
 
   return (
     <div className="App">
       <h1>{user}</h1>
       {username ? <h1>Welcome {username}</h1> : <h1>Please log in</h1>}
       {username && <h2>Success</h2>}
-      <Login setter={setUser} handler={submit_handler} />
+      <Login setter={setUser} handler={submitHandler} />
       {arr.map((item, i) => {
-        return(
-          <p key={i}>{item}</p>
-        )
+        return (
+          <div>
+            <p key={i}>{item.author}</p>
+            <img
+              className="imageFunTime"
+              src={item.download_url}
+              alt="random"
+            />
+          </div>
+        );
       })}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/login.js
+++ src/components/login.js
@@ -1,10 +1,7 @@
 export const Login = ({setter, handler}) => {
     return(
         <form onSubmit={handler}>
-        <input 
-          placeholder="username" 
-          onChange={(e) => setter(e.target.value)}
-        />
+        <input placeholder="username" onChange={(e) => setter(e.target.value)}/>
       </form>
     )
 }
