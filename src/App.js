837fe16868ed14a8204e620c93e35d391fcaf76d
GWeightman <g.weightman@hotmail.co.uk>
2022-01-11 19:07:27 +0000

    func updates

--- src/App.js
+++ src/App.js
@@ -1,30 +1,81 @@
 import React, { useState } from "react";
 import "./App.css";
-import { signup_fetch } from "./util";
+import { signup_fetch, remove_fetch, edit_fetch, info_fetch } from "./util";
 
 const App = () => {
   const [user, setUser] = useState();
+  const [del, setDel] = useState()
+  const [edit, setEdit] = useState()
+  const [info, setInfo] = useState()
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
 
   const signup_handler = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const return_value = await signup_fetch(username, password);
+    setUser(return_value.user_entry.username);
+    console.log(return_value);
+  }
+
+  const remove_handler = async (e) => {
+    e.preventDefault();
+    const return_value = await remove_fetch(username);
+    setDel(username);
+    console.log(return_value);
+  }
+
+  const edit_handler = async (e) => {
+    e.preventDefault();
+    const return_value = await edit_fetch(username, password);
+    setEdit(username);
+    console.log(return_value);
+  }
+
+  const info_handler = async (e) => {
+    e.preventDefault();
+    const return_value = await info_fetch(username);
+    setInfo(return_value);
     console.log(return_value);
   }
 
   return (
     <div className="App">
-      <h1>{user}</h1>
-      {!user ? (
-        <form onSubmit={signup_handler}>
+      <h2>Log In</h2>
+      {user ? (<p>{user} has logged in</p>) : ("")}
+      <form onSubmit={signup_handler}>
           <input onChange={(e) => setUsername(e.target.value)} placeholder="username"/>
           <input onChange={(e) => setPassword(e.target.value)} placeholder="password"/>
           <button type="submit">Submit</button>
-        </form>
-      ) : (
-        <h2>You are logged in</h2>
-      )}
+      </form>
+
+      <br/>
+
+      <h2>Remove</h2>
+      {del ? (<p>{del} has been deleted</p>) : ("")}
+      <form onSubmit={remove_handler}>
+          <input onChange={(e) => setUsername(e.target.value)} placeholder="username"/>
+          <button type="submit">Submit</button>
+      </form>
+
+      <br/>
+
+      <h2>Edit</h2>
+      {edit ? (<p>{edit} has been updated</p>) : ("")}
+      <form onSubmit={edit_handler}>
+          <input onChange={(e) => setUsername(e.target.value)} placeholder="username"/>
+          <input onChange={(e) => setPassword(e.target.value)} placeholder="password"/>
+          <button type="submit">Submit</button>
+      </form>
+
+      <br/>
+
+      <h2>info</h2>
+      {info ? (<p>Username:{info.username}</p>) : ("")}
+      {info ? (<p>Password:{info.password}</p>) : ("")}
+      <form onSubmit={info_handler}>
+          <input onChange={(e) => setUsername(e.target.value)} placeholder="username"/>
+          <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
--- src/util/index.js
+++ src/util/index.js
@@ -14,3 +14,48 @@ export const signup_fetch = async (username, password) => {
         console.log(error)
     }
 }
+
+export const remove_fetch = async (username) => {
+    try {
+        const response = await fetch(`${process.env.REACT_APP_REST_API}user/${username}`, {
+            method: "DELETE",
+            headers: {"Content-Type": "application/json"},
+            body: ""
+        });
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const edit_fetch = async (username, password) => {
+    try {
+        const response = await fetch(`${process.env.REACT_APP_REST_API}user`, {
+            method: "PUT",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                username,
+                password
+            }),
+        });
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const info_fetch = async (username) => {
+    try {
+        const response = await fetch(`${process.env.REACT_APP_REST_API}user/${username}`, {
+            method: "GET",
+            headers: {"Content-Type": "application/json"},
+            
+        });
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(error)
+    }
+}
